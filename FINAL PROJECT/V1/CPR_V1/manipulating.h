95080aaf070af4236ff346de63246c1cb0cfe8cd
Chaerin <cyoo10@myseneca.ca>
2024-09-10 01:26:44 -0400

    CPR assignment

---
+++ FINAL PROJECT/V1/CPR_V1/manipulating.h
@@ -0,0 +1,6 @@
+#ifndef _MANIPULATING_H
+#define _MANIPULATING_H
+#include <stdio.h>
+#include <string.h>
+void manipulating(void);
+#endif
---
+++ FINAL PROJECT/V1/CPR_V1/tokenizing.h
@@ -0,0 +1,14 @@
+//
+//  tokenizing.h
+//  Tokenizing
+//
+//  Created by Mark Legrove on 2024-03-31.
+//
+
+#ifndef _TOKENIZING_H_
+#define _TOKENIZING_H_
+#include <stdio.h>
+#include <string.h>
+void tokenizing(void);
+
+#endif /* tokenizing_h */
